a1e2195024ee437dd995839d40f5e99a114ea184
hridyadivakaran <divahri1294@gmail.com>
2016-11-05 14:23:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,18 @@ var button = document.getElementById("counter");
 var counter = 0;
 button.onclick = function() {
     
+    var request = new XMLhttpRequest();
+    
+    request.onreadystateChange = function() {
+     if(request.readyState == XMLhttpRequest.DONE) {
+      if(request.status == 200){
+          var counter = request.responseText;
+          var span = document.getElementById('count');
+          span.innerHTML = counter.toString();
+      }
+     }
+    };
+    
     counter = counter + 1;
     var span = document.getElementById("count");
     span.innerHTML = counter.toString();
