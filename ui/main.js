cca33719ba5308620d5b927ace5809e553e6900f
hridyadivakaran <divahri1294@gmail.com>
2016-11-05 10:45:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,3 +5,12 @@ var element = document.getElementById("main-text"
  );
  
  element.innerHTML = "new value";
+ 
+ //move the dino
+ 
+ var img = document.getElementById("dino"
+ );
+ img.onclick = function()  {
+     
+   img.style.marginleft = '100px';  
+ };
