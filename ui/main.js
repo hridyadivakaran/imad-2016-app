7ebbc61f00e627ab2e4ac03f7ccbbe6311a5a13c
hridyadivakaran <divahri1294@gmail.com>
2016-11-09 22:11:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,20 +9,14 @@ button.onclick = function() {
     request.onreadystateChange = function() {
      if(request.readyState === XMLhttpRequest.DONE) {
       if(request.status === 200){
-           var name = request.responseText;
-           name = JSON.parse(names);
-    var list ='';
-    for(var i=0; i<names.length; i++){
-        list += '<li>' + names[i] + '</li>';
-        
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+           var counter = request.responseText;
+           var span = document.getElementById('count');
+           span.innerHTML = counter.toString();
       }
      }
     };
     
-    request.open('GET','http://hridyadivakaran.imad.hasura-app.io/Submit-name?name=' + name, true);
+    request.open('GET','http://hridyadivakaran.imad.hasura-app.io/counter', true);
     request.send(null);
 };
 
@@ -30,6 +24,28 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var Submit = document.getElementById('Submit_btn');
 Submit.onclick = function() {
+     var request = new XMLhttpRequest();
     
+    request.onreadystateChange = function() {
+     if(request.readyState === XMLhttpRequest.DONE) {
+      if(request.status === 200){
+          var name = request.responseText;
+           name = JSON.parse(names);
+           var list ='';
+           for(var i=0; i<names.length; i++){
+        list += '<li>' + names[i] + '</li>';
+        
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
    
+           
+      }
+     }
+    };
+    
+    request.open('GET','http://hridyadivakaran.imad.hasura-app.io/Submit-name?name=' + name, true);
+    request.send(null);
+    
+    
 };
