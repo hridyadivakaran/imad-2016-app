8ccfdb8d786064a94b951b1938fa6d82cdd77479
hridyadivakaran <divahri1294@gmail.com>
2016-11-22 20:41:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var Pool = require('pg').Pool;
+//var Pool = require('pg').Pool;
 
-var config = {
- host: 'db.imad.hasura-app.io',
- user: 'hridyadivakaran',
- password: process.env,DB_PASSWORD,
- database: 'hridyadivakaran',
- port: '5432'
-};
+//var config = {
+ //host: 'db.imad.hasura-app.io',
+ //user: 'hridyadivakaran',
+ //password: process.env,DB_PASSWORD,
+ //database: 'hridyadivakaran',
+ //port: '5432'
+//};
 var app = express();
 app.use(morgan('combined'));
 
@@ -92,17 +92,17 @@ app.get('/', function (req, res) {
 });
 
 
-var pool = new Pool(config);
-app.get('/test-db', function (req, res){
-     pool.query('SELECT * FROM test', function(err, result) {
+//var pool = new Pool(config);
+//app.get('/test-db', function (req, res){
+    // pool.query('SELECT * FROM test', function(err, result) {
       // handle an error from the query
-      if(err) {
-          res.status(500).send(err.toString());
-      }else{
-      res.send(JSON.stringify(result));
-      }
-    });
-});
+      //if(err) {
+        //  res.status(500).send(err.toString());
+     // }else{
+      //res.send(JSON.stringify(result));
+      //}
+    //});
+//});
 
 var counter = 0;
 app.get('/counter', function(req, res){
