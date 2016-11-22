48365820c2521e5d5de1d2a6dfb25ff2e7387a26
hridyadivakaran <divahri1294@gmail.com>
2016-11-22 22:12:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,17 +92,17 @@ app.get('/', function (req, res) {
 });
 
 
-//var pool = new Pool(config);
-//app.get('/ test-db' , function (req, res){
-    // pool.query('SELECT * FROM test', function(err, result) {
-      // handle an error from the query
-      //if(err) {
-        //  res.status(500).send(err.toString());
-     // }else{
-      //res.send(JSON.stringify(result));
-      //}
-    //});
-//});
+var pool = new Pool(config);
+app.get('/', test-db , function (req, res){
+     pool.query('SELECT * FROM test', function(err, result) {
+       //handle an error from the query
+      if(err) {
+          res.status(500).send(err.toString());
+      }else{
+      res.send(JSON.stringify(result));
+      }
+    });
+});
 
 var counter = 0;
 app.get('/counter', function(req, res){
