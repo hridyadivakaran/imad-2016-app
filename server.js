5f9e0f2b488b3d7e1d2bcd6c4d37bef76332c1c0
hridyadivakaran <divahri1294@gmail.com>
2016-11-22 20:27:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,7 +94,15 @@ app.get('/', function (req, res) {
 
 var pool = new Pool(config);
 app.get('/test-db', function (req, res){
-    
+     pool.query('SELECT * FROM test', function(err, result) {
+      // handle an error from the query
+      if(err) {
+          res.status;
+      }else{
+      res.writeHead(200, {'content-type': 'text/plain'});
+      res.end('You are visitor number ' + result.rows[0].count);
+      }
+    });
 });
 
 var counter = 0;
