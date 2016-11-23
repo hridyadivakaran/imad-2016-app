2d384ec9f51ab63579c45dcaa664d2e0545ea710
hridyadivakaran <divahri1294@gmail.com>
2016-11-23 09:39:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -118,10 +118,22 @@ app.get('/Submit-name', function (req, res){
    res.send(JSON.stringify(names));
 });
 
-app.get('/articleName', function (req, res){
+app.get('/article/:articleName', function (req, res){
     var articleName = req.params.articleName;
-    res.send(createTemplate(article[articleName]));
-});
+     pool.query("SELECT * FROM artilce WHERE title = " + req.params.articleName , function(err, result) {
+       
+      if(err) {
+          res.status(500).send(err.toString());
+      }else{
+          if(result.rows.length === 0){
+              res.status(404).send('Article not found');
+          }else{
+             var articleData = result.rows[0];
+             res.send(createTemplate(articleData));
+          }
+      }
+    });
+    });
 
 
 
