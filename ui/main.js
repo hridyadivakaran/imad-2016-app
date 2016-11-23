701d06c0026734710333e30a79b32e972f571a17
hridyadivakaran <divahri1294@gmail.com>
2016-11-23 16:31:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,16 +22,6 @@ button.onclick = function() {
 
 function goToAboutUS() {
     var request = new XMLhttpRequest();
-    request.onreadystateChange = function() {
-     if(request.readyState === XMLhttpRequest.DONE) {
-      if(request.status === 200){
-           var counter = request.responseText;
-           var span = document.getElementById('count');
-           span.innerHTML = counter.toString();
-      }
-     }
-    };
-    
     request.open('GET','http://hridyadivakaran.imad.hasura-app.io/counter', true);
     request.send(null);
 }
