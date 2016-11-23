5a6cb450c5b1bff01d32c6f5ebbc60fe0c9a7b9a
hridyadivakaran <divahri1294@gmail.com>
2016-11-23 17:18:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -150,6 +150,10 @@ app.get('/ui/aboutUS.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'aboutUS.html'));
 });
 
+app.get('/ui/index.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
