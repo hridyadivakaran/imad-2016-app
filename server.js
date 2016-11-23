84f982e77b89997b1c40e39f1e26a49945008df8
hridyadivakaran <divahri1294@gmail.com>
2016-11-24 00:27:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -105,7 +105,13 @@ app.get('/test-db' , function (req, res){
     });
 });
 
-
+var pool = new Pool(config);
+app.get('/user' , function (req, res){
+pool.query('INSERT INTO user (name,email,username,password) VALUES (sree,sree123,sfre,fer)', function(err) {
+    if (err) console.log('error');
+    else console.log('no error');
+  });
+});
     
 var counter = 0;
 app.get('/counter', function(req, res){
