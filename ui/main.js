4fad9dd37ebf0dc90eaac08a0913122789b2969f
hridyadivakaran <divahri1294@gmail.com>
2016-11-26 08:53:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -54,10 +54,10 @@ function createAccount () {
       "url": "http://hridyadivakaran.imad.hasura-app.io/register-me",
       "method": "POST",
       "data":{
-          "name": document.getElementById('name').value,
-          "email" : document.getElementById('email').value,
-          "username" : document.getElementById('username').value,
-          "password" : document.getElementById('password').value
+          "name": name.value,
+          "email" : email.value,
+          "username" : username.value,
+          "password" : password.value
       },
       "headers": {
         "content-type": "application/json",
