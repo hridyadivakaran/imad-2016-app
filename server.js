87b7e71d6cfb2410a85afe77c2dd4d13203c2446
hridyadivakaran <divahri1294@gmail.com>
2016-11-26 12:13:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -123,7 +123,7 @@ function doDBCall (req,res) {
         else count = result.rows[0].count;
       });
      count++;
-    pool.query('INSERT INTO register VALUES (count,'+req.body.name+','+req.body.email+','+req.body.username+','+req.body.password+')', function(err) {
+    pool.query('INSERT INTO register VALUES ('+count+','+req.body.name+','+req.body.email+','+req.body.username+','+req.body.password+')', function(err) {
         if(err) return onError(err);
         else console.log('noerror');
       });
